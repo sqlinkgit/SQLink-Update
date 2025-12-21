ae1e1f5cf3c5f611545fbe4a1c024bb4044e2b6e
sqlinkgit <sqlink.git@gmail.com>
2025-12-21 18:03:11 +0100

    V1.0.2

--- index.php
+++ index.php
@@ -199,8 +199,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotspot SQ7UTP</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Hotspot <?php echo $vals['Callsign']; ?></title>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
@@ -316,7 +316,7 @@
     const GLOBAL_CALLSIGN = "<?php echo $vals['Callsign']; ?>";
 </script>
 
-<script src="script.js"></script>
+<script src="script.js?v=<?php echo time(); ?>"></script>
 
 </body>
 </html>
